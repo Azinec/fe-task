8de832d5ce464158d09a119d29a4a884382eb1ea
Inga Lekhman <inga.lekhman@gmail.com>
2018-08-09 16:45:47 +0300

    refresh

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,52 +1,52 @@
-(function($){
+(function($) {
 	var activeTags = [];
+
 	sendAPIRequest();
-	
+
 	function sendAPIRequest(tags) {
 		var params = {
-			"maxresult": 20,
+			'maxresult': 20
 		};
-		
+
 		if (tags && tags.length) {
-			params["tags.label"] = tags.join(",");
+			params['tags.label'] = tags.join(',');
 		}
 		$.ajax({
-			type: "GET",
-			url: "/inventory/api/esearch/filter",
+			type: 'GET',
+			url: '/inventory/api/esearch/filter',
 			data: params,
-			success: function (response) {
+			success: function(response) {
 				successHandler(response);
 			}
 		});
 	}
-	
+
 	function successHandler(response) {
 		if (!response || !response.length) {
 			return;
 		}
-		$(".result").empty();
-		response.forEach(function (element) {
-			$(".result").append("<div class='col-md-3 col-sm-4 col-xs-6 '>" +
-					"<div class='res'><h2 class='title'>" + element.title + "</h2></div></div>");
+		$('.result').empty();
+		response.forEach(function(element) {
+			$('.result').append('<div class="col-md-3 col-sm-4 col-xs-6 ">' +
+					'<div class="res"><h2 class="title">' + element.title + '</h2></div></div>');
 		});
 	}
-	
-	$(".item").on("click", function () {
-		var  itemTag = $(this).attr("data-type");
-		var  isActive = $(this).hasClass("active");
-		
+
+	$('.item').on('click', function() {
+		var itemTag = $(this).attr('data-type');
+		var isActive = $(this).hasClass('active');
+
 		if (!itemTag) {
 			return;
 		}
-		
 		if (!isActive) {
 			activeTags.push(itemTag);
 		} else {
-			activeTags = activeTags.filter(function (tag) {
+			activeTags = activeTags.filter(function(tag) {
 				return tag !== itemTag;
 			});
 		}
-		$(this).toggleClass("active", !isActive);
+		$(this).toggleClass('active', !isActive);
 		sendAPIRequest(activeTags);
 	});
 })(jQuery);
