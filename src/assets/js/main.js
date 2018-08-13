5b801af535b1b8c8a8952f8b09c5f80e00a0acc2
Bogdan Serhienko <bodis90@gmail.com>
2018-08-13 15:22:01 +0300

    Small refactior and add link to result items

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,57 +1,62 @@
 (function($) {
-	var activeTags = [];
+  var activeTags = [];
 
-	sendAPIRequest();
+  sendAPIRequest();
 
-	function sendAPIRequest(tags) {
-		var params = {
-			'maxresult': 20
-		};
+  function sendAPIRequest(tags) {
+    var params = {
+      'maxresult': 20
+    };
 
-		if (tags && tags.length) {
-			params['tags.label'] = tags.join(',');
-		}
-		$.ajax({
-			type: 'GET',
-			url: '/inventory/api/esearch/filter',
-			data: params,
-			success: function(response) {
-				successHandler(response);
-			}
-		});
-	}
+    if (tags && tags.length) {
+      params['tags.label'] = tags.join(',');
+    }
+    $.ajax({
+      type: 'GET',
+      url: '/inventory/api/esearch/filter',
+      data: params,
+      success: function(response) {
+        successHandler(response);
+      }
+    });
+  }
 
-	function successHandler(response) {
-		if (!response || !response.length) {
-			return;
-		}
-		$('.result').empty();
-		response.forEach(function(element) {
-		var  thumbnail = 'assets/images/no-image.png';
+  function successHandler(response) {
+    if (!response || !response.length) {
+      return;
+    }
 
-		if  ( element.thumbnails.length > 0 ) {
-				thumbnail = element.thumbnails[0].mediaUrl;
-				}
-		$('.result').append('<div class="col-md-3 col-sm-4 col-xs-6 "><h2 class="title">' + element.title + '</h2>' +
-				'<div class="result-item" style="background-image: url(' + thumbnail + '); "></div></div>');
-		});
-	}
+    $('.result').empty();
+    response.forEach(function(element) {
 
-	$('.item').on('click', function() {
-		var itemTag = $(this).attr('data-type');
-		var isActive = $(this).hasClass('active');
+      var thumbnail = 'assets/images/no-image.png';
 
-	  if (!itemTag) {
-			return;
-		}
-		if (!isActive) {
-			activeTags.push(itemTag);
-		} else {
-			activeTags = activeTags.filter(function(tag) {
-				return tag !== itemTag;
-			});
-		}
-		$(this).toggleClass('active', !isActive);
-		sendAPIRequest(activeTags);
-	});
+
+      if (element.thumbnails.length > 0) {
+        thumbnail = element.thumbnails[0].mediaUrl;
+      }
+
+      $('.result').append('<a class="col-md-3 col-sm-4 col-xs-6 " target="_blank" href="https://www.merolt.de/events-inventory/?squid=' + element.squid + '">' +
+        '<h2 class="title">' + element.title + '</h2>' +
+        '<div class="result-item" style="background-image: url(' + thumbnail + '); "></div></a>');
+    });
+  }
+
+  $('.item').on('click', function() {
+    var itemTag = $(this).attr('data-type');
+    var isActive = $(this).hasClass('active');
+
+    if (!itemTag) {
+      return;
+    }
+    if (!isActive) {
+      activeTags.push(itemTag);
+    } else {
+      activeTags = activeTags.filter(function(tag) {
+        return tag !== itemTag;
+      });
+    }
+    $(this).toggleClass('active', !isActive);
+    sendAPIRequest(activeTags);
+  });
 })(jQuery);
